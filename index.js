54354d35e077b1417c3d1792206cdf7bf5bde41f
Matthijs Booman <matthijs.booman@hotmail.com>
2017-10-02 17:29:28 +0200

    fixed scripts to get full path of target folder

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('test');
+(function(){
+	const path = require('path').normalize(process.argv[2]);
+	//const express = require('express');
+
+	console.log(path);
+})();
--- install.js
+++ install.js
@@ -3,21 +3,8 @@
 		srwsDir = __dirname,
 		scriptFile = ".\\script\\srws";
 
-	var winScript =  `
-@echo off\r\n
-\r\n
-set cwd=%cd%\r\n
-set srwsDir="`+srwsDir+`"\r\n
-\r\n
-cd /d %srwsDir% && node index %*\r\n
-cd /d %cwd%\r\n
-\r\n
-set cwd=\r\n
-set srwsDir=\r\n`,
-		linScript = `
-function homestead() {\r\n
-    ( cd "`+srwsDir+`" && node index $* )\r\n
-}\r\n`;
+	var winScript = '@echo off\r\n\r\nset cwd=%cd%\r\nset srwsDir="'+srwsDir+'"\r\n\r\ncd /d %srwsDir% && node index %cwd%\\%*\r\ncd /d %cwd%\r\n\r\nset cwd=\r\nset srwsDir=\r\n',
+		linScript = 'function homestead() {\r\n\t( cd "'+srwsDir+'" && node index $* )\r\n}\r\n';
 
 	fs.mkdir('.\\script', () => {
 		fs.writeFile(scriptFile + '.bat', winScript, (err) => {
