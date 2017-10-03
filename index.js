bbb85f2f467ed2d11e0e30a7f5907a41788b43cf
Matthijs Booman <matthijs.booman@hotmail.com>
2017-10-03 19:29:46 +0200

    finished v1.0

--- index.js
+++ index.js
@@ -4,10 +4,24 @@
 		port = 8000,
 		app = express();
 
-	app.static(path);
-	app.listen(port);
-
 	app.use(express.static(path));
+	var server = app.listen(port, () => {
+		console.log('> Started server in', path, 'and on localhost:' + port);
+	});
 
-	console.log(process.argv[2], '|', path);
+	process.stdin.addListener("data", function(d) {
+		cmd = d.toString().trim();
+
+	    if(cmd === 'stop') {
+	    	console.log('> Stopping server when clients are disconnected.');
+	    	server.close(() => {
+	    		console.log('> Server stopped.');
+	    		process.exit(0);
+	    	});
+	    } else if(cmd === 'kill') {
+	    	process.exit(0);
+	    }else {
+	    	console.log('> Command', '"'+cmd+'"', 'does not exist.\n  Available commands: stop.');
+	    }
+	});
 })();
--- install.js
+++ install.js
@@ -5,12 +5,12 @@
 		scriptFile = normalize("script/srws");
 
 	var winScript = '@echo off\r\n\r\nset cwd=%cd%\r\nset srwsDir="'+srwsDir+'"\r\n\r\ncd /d %srwsDir% && node index %cwd%\\%*\r\ncd /d %cwd%\r\n\r\nset cwd=\r\nset srwsDir=\r\n',
-		linScript = '( cd "'+srwsDir+'" && node index $curDir/$* )\n';
+		linScript = '( cd "'+srwsDir+'" && node index $(pwd)/$* )\n';
 
 	fs.mkdir('script', () => {
 		fs.writeFile(scriptFile + '.bat', winScript, (err) => {
 			if(err) throw err;
-			console.log('Successfully created bat file');
+			console.log('Successfully created batch file');
 		});
 		fs.writeFile(scriptFile, linScript, (err) => {
 			if(err) throw err;
