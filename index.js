f29c8660afe749e3ac08cb5b02154b6e815c6d87
Mattbo <matthijs.booman@hotmail.com>
2017-10-03 15:49:58 +0200

    fixed sh install

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 (function(){
-	const path = require('path').normalize(process.argv[2]);
-	//const express = require('express');
+	const path = require('path').normalize(process.argv[2]),
+		express = require('express'),
+		port = 8000,
+		app = express();
+
+	app.static(path);
+	app.listen(port);
+
+	app.use(express.static(path));
 
 	console.log(process.argv[2], '|', path);
 })();
--- install.js
+++ install.js
@@ -1,19 +1,20 @@
 (function(){
 	var fs = require('fs'),
+		normalize = require('path').normalize;
 		srwsDir = __dirname,
-		scriptFile = ".\\script\\srws";
+		scriptFile = normalize("script/srws");
 
 	var winScript = '@echo off\r\n\r\nset cwd=%cd%\r\nset srwsDir="'+srwsDir+'"\r\n\r\ncd /d %srwsDir% && node index %cwd%\\%*\r\ncd /d %cwd%\r\n\r\nset cwd=\r\nset srwsDir=\r\n',
-		linScript = 'function srws() {\r\n\tcurDir=$(pwd)\r\n\t( cd "'+srwsDir+'" && node index $curDir/$* )\r\n}\r\n';
+		linScript = '( cd "'+srwsDir+'" && node index $curDir/$* )\n';
 
-	fs.mkdir('.\\script', () => {
+	fs.mkdir('script', () => {
 		fs.writeFile(scriptFile + '.bat', winScript, (err) => {
 			if(err) throw err;
 			console.log('Successfully created bat file');
 		});
-		fs.writeFile(scriptFile + '.sh', linScript, (err) => {
+		fs.writeFile(scriptFile, linScript, (err) => {
 			if(err) throw err;
-			console.log('Successfully created sh file');
+			console.log('Successfully created shell file');
 		});
 	});
 })();
