d59e77587b803682bc7e135f58d03b76fe92d8d9
Mattbo <matthijs.booman@hotmail.com>
2017-10-04 12:10:09 +0200

    changed script folder to bin

--- install.js
+++ install.js
@@ -1,13 +1,15 @@
 (function(){
-	var fs = require('fs'),
-		normalize = require('path').normalize;
-		srwsDir = __dirname,
-		scriptFile = normalize("script/srws");
+	const cwd = process.cwd();
+	console.log(">>", cwd);
+	if(!cwd.includes('/node_modules/', 8)){
+		var fs = require('fs'),
+			normalize = require('path').normalize;
+			srwsDir = __dirname,
+			scriptFile = normalize("bin/srws");
 
-	var winScript = '@echo off\r\n\r\nset cwd=%cd%\r\nset srwsDir="'+srwsDir+'"\r\n\r\ncd /d %srwsDir% && node index %cwd%\\%*\r\ncd /d %cwd%\r\n\r\nset cwd=\r\nset srwsDir=\r\n',
-		linScript = '( cd "'+srwsDir+'" && node index $(pwd)/$* )\n';
+		var winScript = '@echo off\r\n\r\nset cwd=%cd%\r\nset srwsDir="'+srwsDir+'"\r\n\r\ncd /d %srwsDir% && node index %cwd%\\%*\r\ncd /d %cwd%\r\n\r\nset cwd=\r\nset srwsDir=\r\n',
+			linScript = '( cd "'+srwsDir+'" && node index $(pwd)/$* )\n';
 
-	fs.mkdir('script', () => {
 		fs.writeFile(scriptFile + '.bat', winScript, (err) => {
 			if(err) throw err;
 			console.log('Successfully created batch file');
@@ -16,5 +18,5 @@
 			if(err) throw err;
 			console.log('Successfully created shell file');
 		});
-	});
+	}
 })();
